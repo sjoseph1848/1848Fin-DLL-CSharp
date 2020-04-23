534c498e116d45df163af6522c949ffb1ef03022
Sam <sjoseph1848@gmail.com>
2020-04-22 20:41:58 -0500

    first commit

---
+++ Desktop/1848Fin-Be/Fin.BL/Fin.BL/Models/IncomeStatementDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fin.BL.Models
+{
+    public class IncomeStatementDto
+    {
+        public string Symbol { get; set; }
+
+        public FinancialsDto[] financials { get; set; }
+    }
+}
